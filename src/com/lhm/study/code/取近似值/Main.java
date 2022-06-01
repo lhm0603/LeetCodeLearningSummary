7320720fa21da5dde31198c715ad326c48d76854
lhm0603 <461415832@qq.com>
2022-06-01 18:35:27 +0800

    取近似值

---
+++ src/com/lhm/study/code/取近似值/Main.java
@@ -0,0 +1,11 @@
+package com.lhm.study.code.取近似值;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        float v = input.nextFloat();
+        System.out.println(Math.round(v));
+    }
+}
